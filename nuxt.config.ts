053ff50e2084ae9957c335ea697b8e130fb25b5d
yasinyoruk <yasin.yoruk@despatchcloud.com>
2023-01-13 13:07:46 +0300

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    runtimeConfig: {
+        url: process.env.SUPABASE_URL,
+        key: process.env.SUPABASE_KEY
+    },
+    modules: ['@nuxtjs/supabase'],
+})
